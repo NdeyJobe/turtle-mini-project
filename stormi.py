75eb8ab2cbafd76ae2228ca7a4c5eb9d627c3ea1
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-16 11:18:14 +0100

    Add my initials

---
+++ stormi.py
@@ -0,0 +1,67 @@
+# stormi.py
+
+import turtle
+
+def shap_pic ():
+    glen = turtle.Turtle ()
+    glen.color ("pink")
+    glen.speed (3)
+    glen.shape ("circle")
+    
+    window = turtle.Screen ()
+    window.bgcolor ("red")
+
+    glen.left (90)
+    glen.forward (150)
+    glen.right (150)
+    glen.forward (175)
+    glen.right (210)
+    glen.forward (150)
+
+    glen.right (90)
+    glen.forward (150)
+
+    glen.left (0)
+    glen.backward (75)
+
+    glen.right (90)
+    glen.forward (150)
+
+    glen.left (270)
+    glen.forward (60)
+
+    glen.right (180)
+    glen.forward (120)
+
+    glen.backward (60)
+
+    glen.left (90)
+    glen.forward (150)
+
+    glen.right (90)
+    glen.forward (150)
+
+    glen.right (90)
+    glen.forward (150)
+
+    glen.right (90)
+    glen.forward (70)
+    
+    window.exitonclick()
+
+
+'''def art (trianglen):
+    trianglen = turtle.Turtle ()
+    trianglen.forward(150)
+    trianglen.right (45)
+    trianglen.backward(200)
+    trianglen.right (45)
+    trianglen.forward(150)
+    trianglen.right (45)'''
+
+
+    
+
+shap_pic ()
+
+        
